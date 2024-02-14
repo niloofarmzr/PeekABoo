fba99059f7d9cde79d70a7d60f2a18d9046b9ddc
Niloofar <niloofar@Niloofars-MacBook-Air.local>
2024-02-14 00:09:26 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import PeekABoo from "./components/peekABoo";
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='peekABoo'>
+          <PeekABoo/>
+      </div>
+    </div>
+  )
+}
+
+export default App
